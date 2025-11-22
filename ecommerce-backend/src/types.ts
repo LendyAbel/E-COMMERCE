6bef19a34e7f737c8b9580f9150f9ebf1f4c61ed
LendyAbel <lendyabel93@gmail.com>
2025-11-22 14:14:42 +0100

    [improve] types and data

--- ecommerce-backend/src/types.ts
+++ ecommerce-backend/src/types.ts
@@ -1,24 +1,42 @@
 export interface Product {
-    id: string;
-    sku: string;
-    name:string;
-    slug: string;
-    shortDescription: string;
-    longDescription: string;
-    brand: string;
-    mainCategory: string;
-    subCategory: string[];
-    basePrice: number;
-    taxRate: number;
-    inStock: boolean;
-    stockQuantity: number;
-    status: 'draft' | 'published' | 'hidden' | 'discontinued';
-    mainImage: string;
-    galeryImages: string[];
+  id: string;
+  sku: string;
+  name: string;
+  slug: string;
+  shortDescription: string;
+  longDescription: string;
+  brand: string;
+  mainCategory: ProductCategory['name'];
+  otherCategory: Array<ProductCategory['name']>;
+  price: number;
+  vatType: number;
+  inStock: boolean;
+  stock: number;
+  status: 'draft' | 'published' | 'hidden' | 'discontinued';
+  images: ProductImage[];
+  variants?: ProductVariant[];
+  attributes: Record<string, string | number | boolean>; // {[key: string]: string | number | boolean}
 }
 
-export interface ProductVariant extends Product{
-    variantOf: string;
-    attributes: Record<string, string>;
-    ean?: string;
+export interface ProductCategory {
+  id: string;
+  name: string;
+  slug: string;
+  parentSlug?: string;
+}
+
+export interface ProductImage {
+  url: string;
+  alt: string;
+  isMain: boolean;
+}
+
+export interface ProductVariant {
+  id: string;
+  sku: string;
+  attributes: Record<string, string | number | boolean>; // {[key: string]: string | number | boolean}
+  price: number;
+  stock: number;
+  ean?: string;
+  images?: ProductImage[];
 }
